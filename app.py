321f72f25939ed71612a76a92169c2363cd4c831
Karim Chiouar <karim.chiouar@holmarcom.ma>
2026-06-26 16:09:39 +0100

    Mise a jour avec print fichier modifie pour la deuxieme fois

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import ollama
 from ollama import chat
-print('eeeeeeeeeeee')
+print('ffffffffffffff')
 stream=chat(model='qwen2.5:14b',messages=[{'role':'user','content':"comment calculer le rwa d une banque"}],stream=True)
 for chunk in stream:
     print(chunk['message']['content'],end='',flush=True)
